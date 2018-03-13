475e5493c2496da32be20de661badba2f42041ec
Ethan Peterson <epeterson@rsgc.on.ca>
2018-03-13 16:11:57 -0400

    got block moving in y dir

--- energy.py
+++ energy.py
@@ -26,14 +26,14 @@ blockStartPos = vector(startPos.x + (blockWidth / 2), startPos.y + (blockHeight 
 blockVel = vector(0, 0, 0) # initial velocity of the block (starts at rest)
 
 # use forces to calculate the acceleration down the ramp (resulting velocity will be used to update position and to calculate kinetic energy throughout the motion)
-
+a = vector(0,0,0)
 Fg = vector((blockMass * gravity.y) * numpy.sin(theta), (blockMass * gravity.y) * numpy.cos(theta), blockMass * gravity.y) # z value will simply be overall magnitude of Fg
 # coordinate system is on an angle of 25 degrees so we must use trig to get x and y components of Fg
 # multiplying Fg by sin of the angle for x comp and by cos for y comp
 Fnet = Fg.x # net force is the x component of gravity
-a = Fnet / blockMass # calculate acceleration derived from formula Fnet = ma
-
-
+a.x = Fnet / blockMass # calculate acceleration derived from formula Fnet = ma
+print(a)
+a.y = a.x * numpy.tan(theta) # use trig to inferr acceleration for y - dir since we defined coordinate system on angle
 # Time Related Constants
 
 t = 0 # logs the total time the fall takes in seconds
@@ -44,14 +44,16 @@ dt = 0.01 # deltaT variable used for calculations as it will be the difference i
 # Setup Visuals
 ballScene = display (x=0, y=0, width = 500, height = 500, autoscale = true, background=color.black) # creates the window where our simulation will be shown
 block = box(pos = blockStartPos, size = (blockWidth, blockHeight, 0), material = materials.marble) # creates the ground with the earth texture, which the ball will fall towards
-cylinder(pos=(0, 0, 0), axis=(5,0,0), radius=0.1, color = color.white) # base
-cylinder(pos=(0, 0, 0), axis=startPos, radius=0.1, color = color.white) # slope
+base = cylinder(pos=(0, 0, 0), axis=(5,0,0), radius=0.1, color = color.white) # base
+slope = cylinder(pos=(0, 0, 0), axis=startPos, radius=0.1, color = color.white) # slope
 #ball = sphere(pos = ballStartPos, radius = 10, material = materials.marble) # creates the ball, which will fall towards the ground
 while True:
     rate(200)
     if block.pos.x < 0: # When block is sliding down the ramp
-        blockVel.x = blockVel.x + (a * dt)
+        blockVel = blockVel + (a * dt)
+        #blockVel.y = blockVel.y + (a.y * dt)
         block.pos.x += blockVel.x * dt
+        block.pos.y -= blockVel.y * dt 
     else:
         pass
     t += dt
