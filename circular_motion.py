0d17bc460559bcd3182536b5bd09912dff9275cc
Ethan Peterson <epeterson@rsgc.on.ca>
2018-03-13 22:15:21 -0400

    strange velocity values and simulation behaviour some things will be left out until Mr. Baron is consulted

--- circular_motion.py
+++ circular_motion.py
@@ -1,3 +1,8 @@
+# Ethan Peterson
+# Circular Motion Simulation
+# Reference https://github.com/gcschmit/vpython-physics/blob/master/circular%20motion/circularMotion.py
+
+
 # Import Relevant Libraries
 from visual import * # VPython Libs
 import numpy # NumPy lib for access to additional mathematical operations with python 
@@ -16,13 +21,31 @@ theta = 0 # in Rad
 diameter = 2 # the diameter of the ball
 string = cylinder(pos=(0, 0, 0), axis=(0, -length, 0), radius=0.1)
 ball = sphere(pos=(0, -length - 0.5 * diameter, 0), radius=0.5 * diameter, color=color.red)
+period = 1.0 # in seconds
+ballMass = 0.1 # in Kg
+ballW = vector(0, 0, 2 * pi / period) # since omega is equal to 2pi * 1/T
+ballVel = cross(ballW, string.axis) # velocity is the cross product of these two vectors
+radius = length # radius of the circle formed by the motion of the ball
 
 # Time Related Constants
 
 t = 0 # logs the total time the fall takes in seconds
 dt = 0.01 # deltaT variable used for calculations as it will be the difference in time between each time the loop runs
-period = 1.0 # in seconds
 
 while True:
-    rate(200)
-    pass
+    rate(100)
+    # centripetal force is the net force
+    # the force is the triple scalar product of velocity omega and mass
+    Fnet = ballMass * cross(ballW, ballVel)
+
+    # use Newton's Second law to determine acceleration
+    a = Fnet / ballMass
+
+    # Calculate resulting velocity
+    ballVel += a * dt
+
+    # update string axis allowing it to remain connected to the ball
+    string.axis = rotate(string.axis, angle=(mag(ballW) * dt))
+    ball.pos = string.axis
+
+    t += dt
